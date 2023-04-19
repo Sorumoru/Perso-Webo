396fb910c1b97575e3e5b4357c3fc01b85d5e7fc
Jun Earl Solomon <78795888+Sorumoru@users.noreply.github.com>
2023-04-18 18:56:53 -0700

    fancy line movement

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 https://www.w3schools.com/w3css/w3css_tabulators.asp
-function openContent(contentName) {
-  var i;
-  var x = document.getElementsByClassName("content");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function openContent(contentName, itemLine) {
+
+  var contentList = document.getElementsByClassName("content");
+  for (var x = 0; x < contentList.length; x++) {
+    contentList[x].style.display = "none";
   }
   document.getElementById(contentName).style.display = "block";
+
+  var itemList = document.getElementsByClassName("line");
+  for (var x = 0; x < itemList.length; x++) {
+    itemList[x].style.width = "50%";
+  }
+  document.getElementById(itemLine).style.width = "100%";
 }
