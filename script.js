b2dd305a87615141a391d11e524a4c80c1b1fe13
Jun Earl Solomon <78795888+Sorumoru@users.noreply.github.com>
2023-04-18 19:24:09 -0700

    widen line bug fix

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 https://www.w3schools.com/w3css/w3css_tabulators.asp
-function openContent(contentName, itemLine) {
-
+function openContent(contentName) {
   var contentList = document.getElementsByClassName("content");
   for (var x = 0; x < contentList.length; x++) {
     contentList[x].style.display = "none";
   }
   document.getElementById(contentName).style.display = "block";
+}
 
+function widenLine(itemLine) {
   var itemList = document.getElementsByClassName("line");
   for (var x = 0; x < itemList.length; x++) {
-    itemList[x].style.width = "50%";
+    itemList[x].classList.remove("project-item-selected");
   }
-  document.getElementById(itemLine).style.width = "100%";
+  document.getElementById(itemLine).classList.add("project-item-selected")
 }
