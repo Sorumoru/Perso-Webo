4ebc44d629827c5a6b370e2a188e0810c4d6d384
Jun Earl Solomon <78795888+Sorumoru@users.noreply.github.com>
2023-05-06 15:42:37 -0700

    added comments to functions

--- public/script.js
+++ public/script.js
@@ -1,5 +1,10 @@
-https://www.w3schools.com/w3css/w3css_tabulators.asp
-function openContent(contentName) {
+/**
+ * This function displays the content that is selected by the user.
+ * source used: https://www.w3schools.com/w3css/w3css_tabulators.asp
+ * 
+ * @param {string} contentName The class name of the div to be displayed.
+ */
+function displayContent(contentName) {
   var contentList = document.getElementsByClassName("content");
   for (var x = 0; x < contentList.length; x++) {
     contentList[x].style.display = "none";
@@ -7,6 +12,11 @@ function openContent(contentName) {
   document.getElementById(contentName).style.display = "block";
 }
 
+/**
+ * This function widens the hr line that is selected by the user.
+ * 
+ * @param {string} itemLine The class name of the hr to be widened.
+ */
 function widenLine(itemLine) {
   var itemList = document.getElementsByClassName("line");
   for (var x = 0; x < itemList.length; x++) {
@@ -16,5 +26,5 @@ function widenLine(itemLine) {
 }
 
 function changeTheme() {
-  
+
 }
