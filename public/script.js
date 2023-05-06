714098d09c30f7128b61196657bb753897b8cd91
Jun Earl Solomon <78795888+Sorumoru@users.noreply.github.com>
2023-05-06 16:22:49 -0700

    separated functionalities

--- public/script.js
+++ public/script.js
@@ -26,33 +26,44 @@ function widenLine(itemLine) {
 }
 
 /**
- * This function randomly changes the theme and colors of the website on load.
+ * This function produces the different themes and colors and puts them in a
+ * map for easier navigation.
+ * 
+ * @returns An array of maps with differing themes and colors.
+ */
+function produceThemes() {
+  const theme0 = new Map();
+
+  theme0.set('border-theme', 'linear-gradient(75deg, rgba(255, 215, 0, 1) 0%, rgba(255, 165, 0, 1) 13%, rgba(255, 215, 0, 1) 40%, rgba(255, 255, 255, 1) 50%, rgba(255, 215, 0, 1) 60%, rgba(255, 165, 0, 1) 80%, rgba(255, 215, 0, 1) 100%)');
+  theme0.set('bg-color', 'rgb(28, 28, 28)');
+  theme0.set('font-color', 'rgb(255, 255, 255)');
+  theme0.set('font-color-faint', 'rgb(92, 92, 92)');
+
+  const theme1 = new Map();
+
+  theme1.set('border-theme', 'rgb(0, 0, 0)');
+  theme1.set('bg-color', 'rgb(200, 200, 200)');
+  theme1.set('font-color', 'rgb(10, 10, 10)');
+  theme1.set('font-color-faint', 'rgb(100, 100, 100)');
+
+  var themeArray = [theme0, theme1];
+
+  return themeArray;
+}
+
+/**
+ * This function randomly changes the theme and colors of the website.
+ * Should be used on page load.
  */
 function changeTheme() {
   var randomThemeNumber = Math.floor(Math.random() * 2);
 
   var root = document.querySelector(':root');
 
-  const map0 = new Map();
-
-  map0.set('border-theme', 'linear-gradient(75deg, rgba(255, 215, 0, 1) 0%, rgba(255, 165, 0, 1) 13%, rgba(255, 215, 0, 1) 40%, rgba(255, 255, 255, 1) 50%, rgba(255, 215, 0, 1) 60%, rgba(255, 165, 0, 1) 80%, rgba(255, 215, 0, 1) 100%)');
-  map0.set('bg-color', 'rgb(28, 28, 28)');
-  map0.set('font-color', 'rgb(255, 255, 255)');
-  map0.set('font-color-faint', 'rgb(92, 92, 92)');
-
-  const map1 = new Map();
-
-  map1.set('border-theme', 'rgb(0, 0, 0)');
-  map1.set('bg-color', 'rgb(200, 200, 200)');
-  map1.set('font-color', 'rgb(10, 10, 10)');
-  map1.set('font-color-faint', 'rgb(100, 100, 100)');
-
-  var themeArray = [map0, map1]
+  var themeArray = produceThemes();
 
   root.style.setProperty('--border-theme', themeArray[randomThemeNumber].get('border-theme'));
   root.style.setProperty('--bg-color', themeArray[randomThemeNumber].get('bg-color'));
   root.style.setProperty('--font-color', themeArray[randomThemeNumber].get('font-color'));
   root.style.setProperty('--font-color-faint', themeArray[randomThemeNumber].get('font-color-faint'));
-  // root.style.setProperty('--bg-color', 'red');
-
 }
