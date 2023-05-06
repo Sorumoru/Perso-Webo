b1c34a261594338cccdbf4719ab58d5cc2c0a5ff
Jun Earl Solomon <78795888+Sorumoru@users.noreply.github.com>
2023-05-06 16:18:46 -0700

    added changing themes in js

--- public/script.js
+++ public/script.js
@@ -25,6 +25,34 @@ function widenLine(itemLine) {
   document.getElementById(itemLine).classList.add("project-item-selected")
 }
 
+/**
+ * This function randomly changes the theme and colors of the website on load.
+ */
 function changeTheme() {
+  var randomThemeNumber = Math.floor(Math.random() * 2);
+
+  var root = document.querySelector(':root');
+
+  const map0 = new Map();
+
+  map0.set('border-theme', 'linear-gradient(75deg, rgba(255, 215, 0, 1) 0%, rgba(255, 165, 0, 1) 13%, rgba(255, 215, 0, 1) 40%, rgba(255, 255, 255, 1) 50%, rgba(255, 215, 0, 1) 60%, rgba(255, 165, 0, 1) 80%, rgba(255, 215, 0, 1) 100%)');
+  map0.set('bg-color', 'rgb(28, 28, 28)');
+  map0.set('font-color', 'rgb(255, 255, 255)');
+  map0.set('font-color-faint', 'rgb(92, 92, 92)');
+
+  const map1 = new Map();
+
+  map1.set('border-theme', 'rgb(0, 0, 0)');
+  map1.set('bg-color', 'rgb(200, 200, 200)');
+  map1.set('font-color', 'rgb(10, 10, 10)');
+  map1.set('font-color-faint', 'rgb(100, 100, 100)');
+
+  var themeArray = [map0, map1]
+
+  root.style.setProperty('--border-theme', themeArray[randomThemeNumber].get('border-theme'));
+  root.style.setProperty('--bg-color', themeArray[randomThemeNumber].get('bg-color'));
+  root.style.setProperty('--font-color', themeArray[randomThemeNumber].get('font-color'));
+  root.style.setProperty('--font-color-faint', themeArray[randomThemeNumber].get('font-color-faint'));
+  // root.style.setProperty('--bg-color', 'red');
 
 }
